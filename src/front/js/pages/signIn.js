a546ce53007f3fe1876d357e1aae1c4a01c018dd
Sarah Bejerano <bejeranosarah@gmail.com>
2022-01-05 00:35:16 -0500

    Logical process of create profile and sign in

---
+++ src/front/js/pages/signIn.js
@@ -0,0 +1,56 @@
+import React, { useContext, useState, useEffect } from "react";
+import validator from "validator";
+import { useHistory } from "react-router-dom";
+import { Context } from "../store/appContext";
+import "../../styles/signIn.scss";
+import { PageButton } from "../component/pageButton";
+import { Form } from "react-bootstrap";
+
+
+export const SignIn = () => {
+    const history = useHistory();
+    const { store, actions } = useContext(Context);
+    const [email, setEmail] = useState("");
+    const onEmailChanged = (e) => {
+        setEmail(e.target.value)
+    };
+    const [password, setPassword] = useState("");
+    const onPasswordChanged = (e) => {
+        setPassword(e.target.value)
+    };
+    const signIn = () => {
+        if (validator.isEmail(email) && !validator.isEmpty(password)) {
+            actions.signInProfile(email, password);
+
+        }
+    }
+    useEffect(() => {
+        if (store.user !== null) {
+            history.push("/question")
+        }
+    }, [store.user]);
+
+    return (
+        <div className="signInPage">
+            <h2>SIGN IN</h2>
+            <div className="form-floating mb-3">
+                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"
+                    value={email}
+                    onChange={onEmailChanged}
+                />
+                <label htmlFor="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating  mb-3 specialInput">
+                <input type="password" className="form-control" id="floatingPassword" placeholder="Password"
+                    value={password}
+                    onChange={onPasswordChanged} />
+                <label htmlFor="floatingPassword">Password</label>
+            </div>
+            <div className="buttonContainer">
+                <PageButton
+                    text="PLAY NOW!!" variant="primary"
+                    callback={signIn} />
+            </div>
+        </div >
+    );
+};
---
+++ src/front/js/service/avatars.js
@@ -0,0 +1,20 @@
+import MonaLisa from "../../img/MonaLisa.png";
+import AnaMendieta from "../../img/AnaMendieta.png";
+import Dali from "../../img/Dali.png";
+import Duchamp from "../../img/Duchamp.png";
+import FridaKhalo from "../../img/FridaKhalo.png";
+import GeorgiaOkeeffe from "../../img/GeorgiaOkeeffe.png"
+import LouiseBorgeoise from "../../img/LouiseBorgeoisepng.png"
+import VanGogh from "../../img/VanGogh.png"
+
+
+export const avatars = {
+    monaLisa: MonaLisa,
+    anaMendieta: AnaMendieta,
+    dali: Dali,
+    duchamp: Duchamp,
+    fridaKhalo: FridaKhalo,
+    georgiaOkeeffe: GeorgiaOkeeffe,
+    louiseBorgeoise: LouiseBorgeoise,
+    vanGogh: VanGogh,
+}
