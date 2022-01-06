0a096a2abad54972335afcd9ae542513b59cfcb8
Sarah Bejerano <bejeranosarah@gmail.com>
2022-01-05 19:00:10 -0500

    Style home page and add animation

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,14 +1,21 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.scss";
-import { PageButton } from "../component/pageButton";
+import { LinkButton } from "../component/linkButton";
+import WelcomeAnimation from "../component/welcomeAnimation";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="homeBody">
-			<PageButton text="next" variant="primary" callback={() => { console.log("text") }} />
+			<WelcomeAnimation className="welcomeAnimation" />
+			<div className="buttonContainer">
+				<LinkButton text="Create Profile" variant="secondary" link="/createprofile" />
+			</div>
+			<div className="buttonContainer last">
+				<LinkButton text="Sign In" variant="primary" link="/signin" />
+			</div>
 		</div>
 	);
 };
