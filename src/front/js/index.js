188e8ea0c8a15d8d6581e87587a2287ada378c13
Sarah Bejerano <bejeranosarah@gmail.com>
2022-01-21 18:08:51 -0500

    Cover Image Of Home Page & Nav Bar menu & Logo

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,14 +1,9 @@
-//import react into the bundle
+
 import React from "react";
 import ReactDOM from "react-dom";
 require("./service/firebase");
-
-//include your index.scss file into the bundle
-import "../styles/index.scss";
-
-//import your own components
 import Layout from "./layout";
 
-//render your react application
+
 ReactDOM.render(<Layout />, document.querySelector("#app"));
 
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,41 +3,17 @@ import { Context } from "../store/appContext";
 import "../../styles/home.scss";
 import { LinkButton } from "../component/linkButton";
 import { PageButton } from "../component/pageButton";
-import WelcomeAnimation from "../component/welcomeAnimation";
-import { Title } from "../component/title";
+import { PageHeader } from "../component/header";
+
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 	const logout = () => {
 		actions.signOut();
 	}
 	return (
-		// <div className="pageHeader">
-		// 	Header
-		// </div>
-		<div className="homeBody">
-			<Title></Title>
-			<WelcomeAnimation className="welcomeAnimation" />
-			{!store.user ?
-				(
-					<>
-						<div className="buttonContainer">
-							<LinkButton text="Create Profile" variant="secondary" link="/createprofile" />
-						</div>
-						<div className="buttonContainer last">
-							<LinkButton text="Sign In" variant="primary" link="/signin" />
-						</div>
-					</>
-				) : (
-					<>
-						<div className="buttonContainer last">
-							<LinkButton text="Start" variant="primary" link="/intro" />
-						</div>
-						<div className="buttonContainer last">
-							<PageButton text="Sign Out" variant="secondary" callback={logout} />
-						</div>
-					</>
-				)
-			}
+		<div className="homePage">
+			<PageHeader />
 		</div>
+
 	);
 };
