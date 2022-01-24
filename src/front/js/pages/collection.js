7580487d64b7e205c86f0a9431504db9d4b8c3c0
Sarah Bejerano <bejeranosarah@gmail.com>
2022-01-24 17:39:36 -0500

    create-collection-page

---
+++ src/front/js/pages/collection.js
@@ -0,0 +1,68 @@
+import React, { useContext, useState, useEffect } from "react";
+// import validator from "validator";
+// import { useHistory } from "react-router-dom";
+// import { Context } from "../store/appContext";
+// import { PageButton } from "../component/pageButton";
+// import useSWR from "swr";
+import { PageHeader } from "../component/header";
+import { CollectionCard } from "../component/card";
+import { Row, Col, Container, Carousel, Navbar } from "react-bootstrap"
+import "../../styles/collection.scss";
+
+
+export const CollectionPage = () => {
+    // const history = useHistory();
+    // const { store, actions } = useContext(Context);
+    // const params = useParams();
+
+    // useEffect(() => {
+    //     if (store.user === null) {
+    //         history.push("/")
+    //     }
+    // }, [store.user]);
+
+    // const restartScore = () => {
+    //     actions.resetScore();
+    //     history.push("/")
+    // };
+
+    return (
+        <>
+            <div className="collectionPageContainer">
+                <PageHeader />
+                <Row xs={1} md={4} className="cardRow">
+                    {Array.from({ length: 8 }).map((_, idx) => (
+                        <Col className="cardColumn">
+                            <CollectionCard url="/modernism" title="Modernism" image="https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2015/6/18/1434647790018/d0bb6745-5cbe-4f5a-993d-4c9b25ab86b8-2060x1479.jpeg?width=465&quality=45&auto=format&fit=max&dpr=2&s=446f767582a2c8b288be76dd1113a62a" />
+                        </Col>
+                    ))}
+                </Row>
+
+
+                {/* <CollectionCard url="/modernism" title="Modernism" image="https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2015/6/18/1434647790018/d0bb6745-5cbe-4f5a-993d-4c9b25ab86b8-2060x1479.jpeg?width=465&quality=45&auto=format&fit=max&dpr=2&s=446f767582a2c8b288be76dd1113a62a" /> */}
+
+
+                {/* <div className="avatarProfile"> {store.user && <img src={avatars[store.user.avatarID]} />}</div>
+            {store.user && <h2 className="nameProfile">
+                {store.user.profileName}
+            </h2>}
+            <img className="pointsStar" src={star} />
+            <div className="scoreplayer">{store.score}/10
+            </div>
+            <div className="buttonContainer">
+                <PageButton text="finish" variant="secondary"
+                    callback={() => { restartScore() }} />
+            </div> */}
+
+            </div>
+        </>
+    );
+};
+
+// UserProfile.propTypes = {
+//     onQuit: PropTypes.func,
+
+
+// };
+
+
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,21 +1,20 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import "../../styles/home.scss";
-import { LinkButton } from "../component/linkButton";
-import { PageButton } from "../component/pageButton";
 import { PageHeader } from "../component/header";
-import { CollectionCard } from "../component/card";
+// import { CollectionCard } from "../component/card";
+import "../../styles/home.scss";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+	const { actions } = useContext(Context);
 	const logout = () => {
 		actions.signOut();
 	}
 	return (
 		<div className="homePage">
 			<PageHeader />
-			<h1 className="pageTitle">A learning platform about art</h1>
-			<CollectionCard url="/modernism" title="Modernism" image="https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2015/6/18/1434647790018/d0bb6745-5cbe-4f5a-993d-4c9b25ab86b8-2060x1479.jpeg?width=465&quality=45&auto=format&fit=max&dpr=2&s=446f767582a2c8b288be76dd1113a62a" />
+			<h1 className="pageTitle">A learning platform about art
+			</h1>
+
 		</div>
 	);
 };
