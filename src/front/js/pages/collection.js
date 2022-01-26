6a7b7d2d6831c6bd2c5fa50587b2664dfc030bfa
Sarah Bejerano <bejeranosarah@gmail.com>
2022-01-25 21:12:07 -0500

    create collection generator

--- src/front/js/pages/collection.js
+++ src/front/js/pages/collection.js
@@ -1,12 +1,9 @@
 import React, { useContext, useState, useEffect } from "react";
-// import validator from "validator";
-// import { useHistory } from "react-router-dom";
-// import { Context } from "../store/appContext";
-// import useSWR from "swr";
 import { PageHeader } from "../component/header";
 import { CollectionCard } from "../component/card";
 import { Row, Col } from "react-bootstrap"
 import "../../styles/collection.scss";
+import { ArtPeriods } from "../service/collectionGenerator";
 
 
 export const CollectionPage = () => {
@@ -15,39 +12,18 @@ export const CollectionPage = () => {
         <>
             <div className="collectionPageContainer">
                 <PageHeader />
-                <Row xs={1} md={4} className="cardRow">
-                    {Array.from({ length: 8 }).map((_, idx) => (
-                        <Col className="cardColumn">
-                            <CollectionCard url="/detail/modernism" title="Modernism" year="1910" image="https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2015/6/18/1434647790018/d0bb6745-5cbe-4f5a-993d-4c9b25ab86b8-2060x1479.jpeg?width=465&quality=45&auto=format&fit=max&dpr=2&s=446f767582a2c8b288be76dd1113a62a" />
+                <Row xs={1} md={3} className="cardRow">
+                    {ArtPeriods.map((artPeriod, idx) => (
+                        <Col key={idx} className="cardColumn">
+                            <CollectionCard url={"/detail/" + artPeriod.title} title={artPeriod.title} year={artPeriod.year} image={artPeriod.artworks[0].image_url} />
                         </Col>
                     ))}
                 </Row>
-
-
-                {/* <CollectionCard url="/modernism" title="Modernism" image="https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2015/6/18/1434647790018/d0bb6745-5cbe-4f5a-993d-4c9b25ab86b8-2060x1479.jpeg?width=465&quality=45&auto=format&fit=max&dpr=2&s=446f767582a2c8b288be76dd1113a62a" /> */}
-
-
-                {/* <div className="avatarProfile"> {store.user && <img src={avatars[store.user.avatarID]} />}</div>
-            {store.user && <h2 className="nameProfile">
-                {store.user.profileName}
-            </h2>}
-            <img className="pointsStar" src={star} />
-            <div className="scoreplayer">{store.score}/10
-            </div>
-            <div className="buttonContainer">
-                <PageButton text="finish" variant="secondary"
-                    callback={() => { restartScore() }} />
-            </div> */}
 
             </div>
         </>
     );
 };
 
-// UserProfile.propTypes = {
-//     onQuit: PropTypes.func,
-
-
-// };
 
 
