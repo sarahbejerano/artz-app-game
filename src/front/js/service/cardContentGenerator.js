ed7a2c20a7869fb3614c6b8d138f113c240e0063
Sarah Bejerano <bejeranosarah@gmail.com>
2022-02-01 23:37:42 -0500

    create links in collection detail

--- src/front/js/service/cardContentGenerator.js
+++ src/front/js/service/cardContentGenerator.js
@@ -4,7 +4,7 @@ const imageParams = "/full/843,/0/default.jpg";
 
 export const getArtworks = (query) => {
     const encodedSearchParams = encodeURIComponent(JSON.stringify(query));
-    return fetch("https://api.artic.edu/api/v1/artworks/search?limit=100&fields=artist_title,id,title,image_id,style_title,date_end,place_of_origin,medium_display,dimensions,thumbnail&params=" + encodedSearchParams)
+    return fetch("https://api.artic.edu/api/v1/artworks/search?limit=100&fields=artist_title,id,title,image_id,style_title,date_end,place_of_origin,medium_display,dimensions,thumbnail,artist_ids&params=" + encodedSearchParams)
         .then(resp => resp.json())
         .then(results => {
             return results.data.map((artwork) => {
